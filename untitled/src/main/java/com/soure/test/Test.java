24fb65e52a7b46bdc0c876250c325062bddfe0f5
wangkang19920101 <1258593044@qq.com>
2020-12-21 19:04:13 +0800

    ceshi1

--- untitled/src/main/java/com/soure/test/Test.java
+++ untitled/src/main/java/com/soure/test/Test.java
@@ -1,4 +1,16 @@
 package com.soure.test;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
 }
